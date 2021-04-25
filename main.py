b55288dd76d0fa8aa5e727acfdc92c1f9539d44e
V Venkata Sree Harsha <63708477+harshavallamkonda@users.noreply.github.com>
2021-04-25 15:11:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-email = input("Enter Your Email: ").strip()
+#Mail-Spatula Created by Harsha Vallamkonda
+
+email = input("Enter Your mail: ").strip()
 username = email[:email.index("@")]
 domain_name = email[email.index("@")+1:]
 format_ = (f"Your user name is '{username}' and your domain is '{domain_name}'")
